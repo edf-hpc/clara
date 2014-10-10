7e17dc6db8ad38f9afcae853e428940839a540ea
Ana Guerrero López <ana@ekaia.org>
2014-10-10 12:28:57 +0200

    Rename important_packages as extra_packages_image. Fix #31
    Install debconf-utils directly since it's needed a few lines later

--- clara/plugins/clara_images.py
+++ clara/plugins/clara_images.py
@@ -163,7 +163,10 @@ def system_install():
 
     mount_chroot()
     run_chroot(["chroot", work_dir, "apt-get", "update"])
-    pkgs = get_from_config("images", "important_packages", dist).split(',')
+    # Add here the must-have packages
+    run_chroot(["chroot", work_dir, "apt-get", "install", "--no-install-recommends", "--yes", "--force-yes", "debconf-utils"])$
+    # Extra packages to be listed in config.ini
+    pkgs = get_from_config("images", "extra_packages_image", dist).split(',')
     run_chroot(["chroot", work_dir, "apt-get", "install", "--no-install-recommends", "--yes", "--force-yes"] + pkgs)
     run_chroot(["chroot", work_dir, "apt-get", "update"])
     # TODO: temporarily disabled until we found out why we need this.
