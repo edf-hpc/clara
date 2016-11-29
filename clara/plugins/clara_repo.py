edf-hpc/clara

commit e9b90670eddccf400f4bf57853def2b2108ab66c

Check if /etc/clara/repos.ini is there before opening it.

clara/plugins/clara_repo.py
@@ -184,6 +184,8 @@ def do_sync(selected_dist, input_suites=[]):
     logging.debug("The suites to sync are: {0}.".format(" ".join(suites)))
 
     # Read /etc/clara/repos.ini
+    if os.path.isfile('/etc/clara/repos.ini'):
+        clara_exit("File /etc/clara/repos.ini not found.")
     repos = ConfigParser.ConfigParser()
     repos.read("/etc/clara/repos.ini")
 

commit faab63e97bf651c731ede36ddd6b792aef215904

'clara repo sync': fix check for repos.ini
This is a fix for a problem introduced in e9b90670

clara/plugins/clara_repo.py
@@ -185,7 +185,7 @@ def do_sync(selected_dist, input_suites=[]):
     logging.debug("The suites to sync are: {0}.".format(" ".join(suites)))
 
     # Read /etc/clara/repos.ini
-    if os.path.isfile('/etc/clara/repos.ini'):
+    if not os.path.isfile('/etc/clara/repos.ini'):
         clara_exit("File /etc/clara/repos.ini not found.")
     repos = ConfigParser.ConfigParser()
     repos.read("/etc/clara/repos.ini")